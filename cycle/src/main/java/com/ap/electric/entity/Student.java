f83e173c584e00631e26ae800b7a4cc362d226ce
akshaypatil13 <akshaypatil13@localhost>
2024-01-29 22:13:30 +0530

    controller added

--- cycle/src/main/java/com/ap/electric/entity/Student.java
+++ cycle/src/main/java/com/ap/electric/entity/Student.java
@@ -1,5 +1,13 @@
 package com.ap.electric.entity;
 
 public class Student {
+	
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 
 }
