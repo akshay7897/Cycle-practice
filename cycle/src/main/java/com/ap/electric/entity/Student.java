87cb2bea2a1155afa7a75e86faa358cf2750c0ef
akshay7897 <119031172+akshay7897@users.noreply.github.com>
2024-02-04 10:44:55 +0530

    Update Student.java

--- cycle/src/main/java/com/ap/electric/entity/Student.java
+++ cycle/src/main/java/com/ap/electric/entity/Student.java
@@ -4,7 +4,7 @@ public class Student {
 	
 	public Student() {
 		// TODO Auto-generated constructor stub
-		//akshay patil
+		//akshay patil  devlop branch
 	}
 	
 	public static void main(String[] args) {
